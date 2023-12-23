c4c9de1a25cf6914c0406026a74a3d770f2583e8
cristiansiguencia69 <132696217+cristiansiguencia69@users.noreply.github.com>
2023-12-23 12:26:08 -0500

    Signed-off-by: cristiansiguencia69 <132696217+cristiansiguencia69@users.noreply.github.com>

---
+++ src/data/contants.ts
@@ -0,0 +1,105 @@
+export const INITIALSERVICES:Services[]=[
+  {
+    id:'0',
+    name:'debes añadir un nombre referente a la consulta',
+    imageUrl:'',
+    date:'12-10-2023',
+    city:'ciudad',
+    description:'descripcion',
+    diagnosticos:[
+      {
+      id:'1000',
+      name:'nombre del animal',
+      date:'22-22',
+      especie:'especie del animal',
+      estado:'nivel de gravedad valorado de 1 a 5',
+      descripcion:'breve diagnostico'
+      }
+    ]
+  }
+  
+]
+export const SERVICES_LIST: Services[] = [
+  {
+    id: '1',
+    name: 'Consultas',
+    imageUrl: 'https://www.zooplus.es/magazine/wp-content/uploads/2018/09/katzen-impfung.jpg',
+    date: '13/12/2023',
+    city: 'Madrid',
+    description: 'Diagnóstico y tratamiento de enfermedades.',
+    diagnosticos: [
+      {
+        id:'1000',
+        name:'nombre del animal',
+        date:'22-22',
+        especie:'especie del animal',
+        estado:'nivel de gravedad valorado de 1 a 5',
+        descripcion:'breve diagnostico'
+        }
+    ]
+  },
+  
+]
+export const POSTS: Post[] = [
+  {
+    id: '1',
+    name: 'vacunas',
+    imageUrl: 'https://www.zooplus.es/magazine/wp-content/uploads/2018/09/katzen-impfung.jpg',
+    date: '13/12/2023',
+    city: 'Chile',
+    description: 'Diagnóstico y tratamiento de enfermedades.',
+ 
+  },
+  {
+    id: '2',
+    name: 'consultas',
+    imageUrl: 'https://img.freepik.com/vector-premium/accesorios-perros-suministros-juguetes-mascotas-tienda-cachorros-productos-alimenticios-veterinarios-productos-cuidado-traje-canino-bozal-protector-ilustracion-vector-caseta-perro-aislada-sobre-fondo-blanco_124507-9485.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697587200&semt=ais',
+    date: '13/12/2023',
+    city: 'México',
+    description: 'Diagnóstico y tratamiento de enfermedades.',
+ 
+  },
+  
+  
+]
+export const VACUNAS: Vacunas[] = [
+  {
+    id: '1',
+    name: 'Vacunas',
+    date: '13/12/2023',
+    city: 'México',
+    imageUrl:'https://www.dogtorscat.com/wp-content/uploads/2019/08/%C3%A1rticulo-3.jpg',
+    description: 'Diagnóstico y tratamiento de enfermedades.',
+    diagnosticos:[{
+      id:'1000',
+      name:'nombre de vacuna',
+      date:'11-11',
+      especie:'especie del animal',
+      estado:'nivel de gravedad valorado de 1 a 5',
+      descripcion:'breve diagnostico'
+      }]
+  },
+  
+]
+export const INITIALVACUNAS: Vacunas[] = [
+  {
+    id: '1',
+    name: 'Vacuna Inicial',
+    date: '13/12/2023',
+    city: 'Quito',
+    description: 'Todo en vacunas.',
+    imageUrl:'https://img.freepik.com/vector-premium/accesorios-perros-suministros-juguetes-mascotas-tienda-cachorros-productos-alimenticios-veterinarios-productos-cuidado-traje-canino-bozal-protector-ilustracion-vector-caseta-perro-aislada-sobre-fondo-blanco_124507-9485.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697587200&semt=ais',
+
+    diagnosticos:[
+      {
+        id:'1000',
+        name:'nombre del animal',
+        especie:'especie del animal',
+        date:'33-33',
+        estado:'nivel de gravedad valorado de 1 a 5',
+        descripcion:'breve diagnostico'
+        }
+    ]
+  },
+  
+]
